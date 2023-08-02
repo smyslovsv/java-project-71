8cdbdf6b74bfa564745c12fdc6e913d95c59d9e4
SmyslovSV <smyslovsv@yandex.ru>
2023-08-03 01:49:04 +0300

    step04 add picocli command -f=format and parameters filepath

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,13 +1,10 @@
 package hexlet.code;
 
-import picocli.CommandLine.Option;
-
 public class Differ {
 
-    @Option(names = {"-h", "--help"}, description = "Show this help message and exit.")
-    boolean help;
-    @Option(names = {"-V", "--version"}, description = "Print version information and exit.")
-    boolean version;
+//    public Differ() {
+//    }
+
     public static String generate() {
         return "Hello, World!";
     }
